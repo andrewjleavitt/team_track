d8d9b446581c9d303c97c91aa20155ab7375e48c
Andy Leavitt <andyleavitt@gmail.com>
2018-03-13 15:57:02 -0600

    clean up extra setup in tests

--- test/system/people_test.rb
+++ test/system/people_test.rb
@@ -1,39 +1,37 @@
-require 'application_system_test_case'
+require "application_system_test_case"
 
 class PeopleTest < ApplicationSystemTestCase
 
   setup do
     @team = create(:team)
-    @person = build(:person)
+    @person = create(:person, name: "Gentleman Steve")
   end
 
-  test 'visiting the index' do
-    create(:person, name: "Gentleman Steve")
+  test "visiting the index" do
     visit people_url
 
-    assert_selector 'h1', text: 'People'
+    assert_selector "h1", text: "People"
     assert_text "Gentleman Steve"
   end
 
-  test 'creating a person' do
+  test "creating a person" do
     visit people_url
-    click_on 'New Person'
-    fill_in 'Name', with: @person.name
-    select @team.name, from: 'person[team_id]'
-    click_on 'Save'
+    click_on "New Person"
+    fill_in "Name", with: @person.name
+    select @team.name, from: "person[team_id]"
+    click_on "Save"
     assert_text @person.name
     assert_text @team.name
   end
 
-  test 'editing a person' do
-    create(:person)
+  test "editing a person" do
     visit people_url
     click_on @person.name
-    click_on 'Edit'
-    fill_in 'Name', with: 'Ben Kenobi'
-    select @team.name, from: 'person[team_id]'
-    click_on 'Save'
-    assert_text 'Ben Kenobi'
+    click_on "Edit"
+    fill_in "Name", with: "Ben Kenobi"
+    select @team.name, from: "person[team_id]"
+    click_on "Save"
+    assert_text "Ben Kenobi"
     assert_text @team.name
   end
 end
