3aa8643eca6b444e6c643dd0ee3d60a109d0c4da
Andy Leavitt <andyleavitt@gmail.com>
2018-03-15 19:17:08 -0600

    add team reference to plans

--- db/migrate/20180307151113_add_project_id_and_team_id_to_plan.rb
+++ db/migrate/20180307151113_add_project_id_and_team_id_to_plan.rb
@@ -1,9 +1,6 @@
-class AddProjectIdToPlan < ActiveRecord::Migration[5.1]
+class AddProjectIdAndTeamIdToPlan < ActiveRecord::Migration[5.1]
   def change
     add_reference :plans, :project, foreign_key: true
-    Plan.all.each do |plan|
-      project = Project.find_by(name: plan.project)
-      plan.update_attributes(project_id: project.id) unless project.nil?
-    end
+    add_reference :plans, :team, foreign_key: true
   end
 end
