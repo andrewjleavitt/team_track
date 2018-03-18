294ef1ea8d9447f0f7656b17e98df361e6ead86a
Andy Leavitt <andyleavitt@gmail.com>
2018-03-18 12:18:49 -0600

    team active assignments, `rake` runs normal and system tests

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,8 @@
 class Team < ApplicationRecord
   has_many :assignments
   has_many :people, through: :assignments
+
+  def active_assignments
+    assignments.active
+  end
 end
--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -1,4 +1,25 @@
 require 'test_helper'
 
 class TeamTest < ActiveSupport::TestCase
+
+  test "#active_assignments" do
+    team = create(:team)
+
+    active_assignment = create(
+      :assignment,
+      person: create(:person),
+      team: team,
+      effective_date: 1.day.ago,
+    )
+
+    inactive_assignment = create(
+      :assignment,
+      person: create(:person),
+      team: team,
+      effective_date: 1.day.from_now,
+    )
+
+    assert_includes team.active_assignments, active_assignment
+    refute_includes team.active_assignments, inactive_assignment
+  end
 end
