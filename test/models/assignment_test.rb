b0ce8e209e8966dbce85c3c970039e9ceaaa3233
Andy Leavitt <andyleavitt@gmail.com>
2018-03-22 09:24:58 -0600

    remove `Assignment.active_assignment_for_person`

--- test/models/assignment_test.rb
+++ test/models/assignment_test.rb
@@ -1,46 +1,6 @@
 require 'test_helper'
 
 class AssignmentTest < ActiveSupport::TestCase
-  test "#active_assignments_for_person" do
-    assigned_person = create(:person)
-    unassigned_person = create(:person)
-    team = create(:team)
-
-    expected_assignment = create(
-      :assignment,
-      team: team,
-      person: assigned_person,
-      effective_date: (Date.today.beginning_of_week - 1.week),
-      completion_date: (Date.today.beginning_of_week + 2.weeks)
-    )
-
-    create(
-      :assignment,
-      team: team,
-      person: assigned_person,
-      effective_date: Date.today.beginning_of_week - 2.weeks,
-      completion_date: Date.today.beginning_of_week - 1.week
-    )
-
-    create(
-      :assignment,
-      team: team,
-      person: unassigned_person,
-      effective_date: Date.today.beginning_of_week - 1.weeks,
-      completion_date: Date.today.beginning_of_week + 2.week
-    )
-
-    other_expected_assignment = create(
-      :assignment,
-      team: team,
-      person: assigned_person,
-      effective_date: Date.today.beginning_of_week - 1.weeks,
-      completion_date: nil
-    )
-
-    assert_equal([expected_assignment, other_expected_assignment], Assignment.active_assignment_for_person(assigned_person))
-  end
-
   test "#active when given a week argument" do
     assignment = create(
       :assignment,
