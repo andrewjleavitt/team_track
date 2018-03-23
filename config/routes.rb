023c208b2884a4e539718b1fc8bb0d53b5c90a68
Gary McKellar <gmckellar@mavenlink.com>
2018-03-23 17:17:27 -0600

    Ignore sessions and registrations; we just want oauth
    Signed-off-by: Brandon Duff <bduff@mavenlink.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }, skip: %i(sessions registrations)
   
   get 'assignments/index'
   root "weekly_plans#index"
