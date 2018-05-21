c4a61fe47a18decd2dc8fa44b921fc5289d6696a
Andy Leavitt <andyleavitt@gmail.com>
2018-05-21 12:25:55 -0600

    fix roles test

--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class RoleTest < ActiveSupport::TestCase
   test "#roles" do
-    expected_roles = ['Engineer', 'Product Manager', 'Team Lead', 'Designer', 'Director']
+    expected_roles = ['Engineer', 'Product Manager', 'Team Lead', 'Designer', 'Director', 'Quality Assurance']
 
     assert_equal(expected_roles, Role.roles)
   end
